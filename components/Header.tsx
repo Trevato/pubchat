ba292a2c1e424935e7c2cbfc7b15cdc9b1dd0f37
Trevor Dobbertin <trevordobbertin@gmail.com>
2023-11-01 23:03:28 -0500

    Commit from Stub AI (#2)

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="text-4xl font-bold text-center py-4">
+      PubChat
+    </header>
+  );
+};
+
+export default Header;
